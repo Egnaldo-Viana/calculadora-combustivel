c057e1af2e81a47a851f65dfe877f6292634006a
Egnaldo Viana <egnaldo.viana.2003@gmail.com>
2025-11-07 00:48:28 -0300

    estilizando projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,44 @@
+import './App.css';
+
+import logoImg from './assets/logo.png';
+
 function App() {
-  return <div>ola mundo</div>;
+  return (
+    <div>
+      <main className="container">
+        <img
+          className="logo"
+          src={logoImg}
+          alt="logo da calculadora  de gasolina ou álcool"
+        />
+        <h1 className="title"> Qual a melhor opção </h1>
+
+        <form className="form">
+          <label> Álcool (preço por litro):</label>
+          <input
+            className="input"
+            type="number"
+            placeholder="4.90"
+            min="1"
+            step="0.01"
+            required
+          />
+
+          <label> Gasolina (preço por litro):</label>
+          <input
+            className="input"
+            type="number"
+            placeholder="4.90"
+            min="1"
+            step="0.01"
+            required
+          />
+
+          <input className="button" type="submit" value="Calcular" />
+        </form>
+      </main>
+    </div>
+  );
 }
 
 export default App;
