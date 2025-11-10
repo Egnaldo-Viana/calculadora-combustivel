797508afe59ab04a84f474e5ce0147d1d121749b
Egnaldo Viana <egnaldo.viana.2003@gmail.com>
2025-11-09 23:54:52 -0300

    implementacao da funcao de calculo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,52 @@
+import React from 'react';
 import './App.css';
 
 import logoImg from './assets/logo.png';
 
+// calculo: alcool / gasolina
+// e se o resultado for menor que 0.7 compensa usar alcool
+
+interface inforProps {
+  title: string;
+  gasolina: string | number;
+  alcool: string | number;
+}
+
 function App() {
+  const [gasolinaInput, setGasolinaInput] = React.useState(0);
+  const [alcoolInput, setAlcoolInput] = React.useState(0);
+  const [info, setInfo] = React.useState<inforProps>();
+
+  function calcular(event: React.FormEvent) {
+    event.preventDefault();
+
+    let calculo = alcoolInput / gasolinaInput;
+    console.log(calculo);
+
+    if (calculo <= 0.7) {
+      setInfo({
+        title: 'Compensa usar álcool',
+        gasolina: formatarMoeda(gasolinaInput),
+        alcool: formatarMoeda(alcoolInput),
+      });
+    } else {
+      setInfo({
+        title: 'Compensa usar gasolina',
+        gasolina: formatarMoeda(gasolinaInput),
+        alcool: formatarMoeda(alcoolInput),
+      });
+    }
+  }
+
+  function formatarMoeda(valor: number) {
+    let valorFormatado = valor.toLocaleString('pt-br', {
+      style: 'currency',
+      currency: 'BRL',
+    });
+
+    return valorFormatado;
+  }
+
   return (
     <div>
       <main className="container">
@@ -13,7 +57,7 @@ function App() {
         />
         <h1 className="title"> Qual a melhor opção </h1>
 
-        <form className="form">
+        <form className="form" onSubmit={calcular}>
           <label> Álcool (preço por litro):</label>
           <input
             className="input"
@@ -21,6 +65,8 @@ function App() {
             placeholder="4.90"
             min="1"
             step="0.01"
+            value={alcoolInput}
+            onChange={(e) => setAlcoolInput(Number(e.target.value))}
             required
           />
 
@@ -31,11 +77,22 @@ function App() {
             placeholder="4.90"
             min="1"
             step="0.01"
+            value={gasolinaInput}
+            onChange={(e) => setGasolinaInput(Number(e.target.value))}
             required
           />
 
           <input className="button" type="submit" value="Calcular" />
         </form>
+
+        {info && Object.keys(info).length > 0 && (
+          <section className="result">
+            <h2 className="result-title">{info.title}</h2>
+
+            <span>Álcool {info.alcool}</span>
+            <span>Gasolina {info.gasolina}</span>
+          </section>
+        )}
       </main>
     </div>
   );
